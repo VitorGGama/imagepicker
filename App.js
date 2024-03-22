88183eb7ebac90e0eb868cc9b4091fe03fdc95ff
Vitor <128196980+VitorGGama@users.noreply.github.com>
2024-03-22 09:15:51 -0300

    UpDate App.js

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
 import { View, StatusBar, Button, Image } from "react-native";
 import { useState, useEffect } from "react";
 
-//Importando os recursos da API nativa/móvel
-import * as Imagepicker from "expo-image-picker";
+/* Importando os recursos da API nativa/móvel */
+import * as ImagePicker from "expo-image-picker";
 
-export default function App(){
-
-  /* State tradicional para armazenar a referencia da foto (quando existir) */
+export default function App() {
+  /* State tradicional para armazenar a referência da foto (quando existir) */
   const [foto, setFoto] = useState(null);
 
-  /* State de checagem de permissões de uso (através do hook useCameraPermissions)*/
-  const [status, requestPermission] = Imagepicker.useCameraPermissions();
+  /* State de checagem de permissões de uso (através do hook useCameraPermission) */
+  const [status, requestPermission] = ImagePicker.useCameraPermissions();
 
   console.log(status);
-}
 
+  /* Ao entrar no app, será executada a verificação de permissões de uso */
+  useEffect(() => {
+    /* Esta função mostrará um popup para o usuário perguntando
+    se ele autoriza a utilização do recurso móvel (no caso, selecionar/tirar foto). */
+    async function verificaPermissoes() {
+      const cameraStatus = await ImagePicker.requestCameraPermissionsAsync();
+
+      /* Ele dando autorização (granted), isso será armazenado
+      no state de requestPermission. */
+      requestPermission(cameraStatus === "granted");
+    }
+
+    verificaPermissoes();
+  }, []);
 
   return (
     <>
